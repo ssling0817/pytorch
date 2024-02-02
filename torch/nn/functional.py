96f8a000d1a232b88ee3c9f60a7fb2e5a5f02886
Hsuan-Ling Huang <ssling0817@gmail.com>
2024-02-02 09:53:02 -0500

    revert change for muti_margin_loss

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -3571,11 +3571,11 @@ def multi_margin_loss(
     input: Tensor,
     target: Tensor,
     p: int = 1,
-    margin: float = 2.0,
+    margin: float = 1.0,
     weight: Optional[Tensor] = None,
     size_average: Optional[bool] = None,
     reduce: Optional[bool] = None,
-    reduction: str = "sum",
+    reduction: str = "mean",
 ) -> Tensor:  # noqa: D400,D402
     r"""multi_margin_loss(input, target, p=1, margin=1, weight=None, size_average=None, reduce=None, reduction='mean') -> Tensor
 
